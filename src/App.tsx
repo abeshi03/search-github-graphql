3f3cde9c32162ed5db785b2c5fed23b45e97c54a
abeshi03 <k_913411@yahoo.co.jp>
2021-08-13 14:45:36 +0900

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import {useQuery} from "@apollo/client";
-import {SEARCH_REPOSITORIES} from "./query/query";
 import React, {useState} from "react";
+import {DefaultState, SearchInput} from "./components/SearchInput";
 
 function App() {
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setQuery({ query: event.target.value });
+    setQuery({
+      ...DEFAULT_STATE,
+      query: event.target.value
+    });
   }
   const PER_PAGE = 5
   const DEFAULT_STATE = {
@@ -15,32 +17,19 @@ function App() {
     before: null,
     query: "フロントエンドエンジニア"
   }
-  type DefaultState = {
-    first?: number;
-    after?: number | null;
-    last?: number | null;
-    before?: number | null;
-    query?: string | undefined;
-  }
   const [{ query, first, last, before, after }, setQuery] = useState<DefaultState>(DEFAULT_STATE)
-  const { loading, error, data } = useQuery(SEARCH_REPOSITORIES, {
-    variables: { query, first, last, before, after }
-  })
 
-  console.log(data)
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
   return (
     <>
       <h1>グラフキューエルのリポジトリ検索アプリ</h1>
       <input value={query} onChange={handleChange}/>
-      {data.search.edges.map((edge: any) => {
-        return (
-          <li key={edge.node.id}>
-            <a href={edge.node.url} target="_blank" rel="noopener noreferrer">{edge.node.name}</a>
-          </li>
-        )
-      })}
+      <SearchInput
+        first={first}
+        query={query}
+        last={last}
+        before={before}
+        after={after}
+      />
     </>
   )
 }
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,32 @@
+import React, {memo, VFC} from "react";
+import {useQuery} from "@apollo/client";
+import {SEARCH_REPOSITORIES} from "../query/query";
+
+export type DefaultState = {
+  first?: number;
+  after?: number | null;
+  last?: number | null;
+  before?: number | null;
+  query?: string | undefined;
+}
+
+export const SearchInput: VFC<DefaultState> = memo((props) => {
+  const { query, first, last, before, after } = props;
+
+  const { loading, error, data } = useQuery(SEARCH_REPOSITORIES, {
+    variables: { query, first, last, before, after }
+  })
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return (
+    <>
+      {data.search.edges.map((edge: any) => {
+        return (
+          <li key={edge.node.id}>
+            <a href={edge.node.url} target="_blank" rel="noopener noreferrer">{edge.node.name}</a>
+          </li>
+        )
+      })}
+    </>
+  );
+});
