04e9b417f2698efc642601e21e42e1dd6fa4d038
abeshi03 <k_913411@yahoo.co.jp>
2021-08-13 14:08:21 +0900

    search ripositore

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-// import {useGetGithubName} from "./hooks/api/useGetGithubName";
-
 import {useQuery} from "@apollo/client";
 import {SEARCH_REPOSITORIES} from "./query/query";
 import React, {useState} from "react";
 
 function App() {
-
-  // const { loading, error, data } = useGetGithubName();
-  //
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error :(</p>;
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setQuery({ query: event.target.value });
@@ -30,12 +23,24 @@ function App() {
     query?: string | undefined;
   }
   const [{ query, first, last, before, after }, setQuery] = useState<DefaultState>(DEFAULT_STATE)
+  const { loading, error, data } = useQuery(SEARCH_REPOSITORIES, {
+    variables: { query, first, last, before, after }
+  })
 
+  console.log(data)
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
   return (
     <>
       <h1>グラフキューエルのリポジトリ検索アプリ</h1>
       <input value={query} onChange={handleChange}/>
-
+      {data.search.edges.map((edge: any) => {
+        return (
+          <li key={edge.node.id}>
+            <a href={edge.node.url} target="_blank" rel="noopener noreferrer">{edge.node.name}</a>
+          </li>
+        )
+      })}
     </>
   )
 }
