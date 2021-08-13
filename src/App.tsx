7f5b3b5fc6e6c3ceef90359a71f0c0789f546347
abeshi03 <k_913411@yahoo.co.jp>
2021-08-13 13:52:07 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import {useGetGithubName} from "./hooks/api/useGetGithubName";
+// import {useGetGithubName} from "./hooks/api/useGetGithubName";
+
+import {useQuery} from "@apollo/client";
+import {SEARCH_REPOSITORIES} from "./query/query";
+import React, {useState} from "react";
 
 function App() {
-  const { loading, error, data } = useGetGithubName();
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+
+  // const { loading, error, data } = useGetGithubName();
+  //
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error :(</p>;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setQuery({ query: event.target.value });
+  }
+  const PER_PAGE = 5
+  const DEFAULT_STATE = {
+    first: PER_PAGE,
+    after: null,
+    last: null,
+    before: null,
+    query: "フロントエンドエンジニア"
+  }
+  type DefaultState = {
+    first?: number;
+    after?: number | null;
+    last?: number | null;
+    before?: number | null;
+    query?: string | undefined;
+  }
+  const [{ query, first, last, before, after }, setQuery] = useState<DefaultState>(DEFAULT_STATE)
+
   return (
     <>
-      <p>{ data.user.name }</p>
-      <img src={data.user.avatarUrl} alt={"写真"}/>
+      <h1>グラフキューエルのリポジトリ検索アプリ</h1>
+      <input value={query} onChange={handleChange}/>
+
     </>
   )
 }
